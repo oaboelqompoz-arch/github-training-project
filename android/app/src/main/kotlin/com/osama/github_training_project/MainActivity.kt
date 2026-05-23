14f527186812d7bf7386230999796bf9065f9f70
oaboelqompoz-arch <oaboelqompoz@smail.ucas.edu.ps>
2026-05-24 00:18:45 +0300

    Initial project setup

---
+++ android/app/src/main/kotlin/com/osama/github_training_project/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.osama.github_training_project
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const GitHubTrainingApp());
+}
+
+class GitHubTrainingApp extends StatelessWidget {
+  const GitHubTrainingApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'GitHub Training',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("GitHub Training Project"),
+        centerTitle: true,
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16),
+        child: Column(
+          children: [
+            Card(
+              child: ListTile(
+                leading: const Icon(Icons.person),
+                title: const Text("Student Name"),
+                subtitle: const Text("ضع اسمك هنا"),
+              ),
+            ),
+            const SizedBox(height: 16),
+            Card(
+              child: ListTile(
+                leading: const Icon(Icons.phone_android),
+                title: const Text("Project Type"),
+                subtitle: const Text("Flutter Application"),
+              ),
+            ),
+            const SizedBox(height: 16),
+            Card(
+              child: ListTile(
+                leading: const Icon(Icons.code),
+                title: const Text("Technologies"),
+                subtitle: const Text("Flutter - Dart - GitHub"),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
